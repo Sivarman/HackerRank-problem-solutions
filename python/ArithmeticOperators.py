fa785ddae49704fc684f3f617221c946c370c9f5
Sivarman <ramangsiva2508@gmail.com>
2025-08-26 19:29:38 +0530

    ➕ HackerRank: Arithmetic Operators – add, subtract, multiply two integers

---
+++ python/ArithmeticOperators.py
@@ -0,0 +1,18 @@
+# Problem: Arithmetic Operators
+# Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# Description:
+# Given two integers a and b, perform the following operations:
+# 1. Add a and b
+# 2. Subtract b from a
+# 3. Multiply a and b
+# Input:
+# Two integers, a and b (1 ≤ a, b ≤ 10^10)
+# Output:
+# Three lines: a + b, a - b, a * b
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
