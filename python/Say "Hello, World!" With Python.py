c72e67bc6f4326714ed6ded2c8410d7f05252bb8
Sivarman <ramangsiva2508@gmail.com>
2025-08-28 20:05:42 +0530

    👋 HackerRank: First Python print statement — Hello, World!

---
+++ python/Say "Hello, World!" With Python.py
@@ -0,0 +1,6 @@
+# Problem: Say "Hello, World!" With Python
+# Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+# Description:
+# Write a single line of code to print "Hello, World!" to standard output.
+
+print("Hello, World!")
